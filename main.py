e919bc11d437e0aa7a78200e7b703064540d01b1
arjavlamsal <lamsalarjav@gmail.com>
2024-10-01 17:10:20 -0500

    game window

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame
 
-pygame.init()
+pygame.init() # Initializing pygame to lead all its components into the program
+
+width = 400
+height = 800
+screen = pygame.display.set_mode((width,height)) # Setting screen resolution (Should pass a tuple as resolution)
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    # Draw all our elements
+    # Update everything
+
+    pygame.display.update() # Updates pygame.display.set_mode((width,height)) so the window keeps on running
+
